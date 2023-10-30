436f10f818b5f001adde62915ded1940448a0506
prabhjyot009 <prabh156dps@gmail.com>
2023-10-30 23:48:41 +0530

    Sensors done

--- controls.js
+++ controls.js
@@ -9,40 +9,37 @@ class Controls{
     }
 
     #addKeyboardListeners(){
-        document.onkeydown=(e)=>{
-            switch(e.key){
-                case "ArrowUp":
-                    this.forward=true;
-                    break;
+        document.onkeydown=(event)=>{
+            switch(event.key){
                 case "ArrowLeft":
                     this.left=true;
                     break;
                 case "ArrowRight":
                     this.right=true;
                     break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
                 case "ArrowDown":
                     this.reverse=true;
                     break;
             }
-            // console.table(this);
         }
-        document.onkeyup=(e)=>{
-            switch(e.key){
-                case "ArrowUp":
-                    this.forward=false;
-                    break;
+        document.onkeyup=(event)=>{
+            switch(event.key){
                 case "ArrowLeft":
                     this.left=false;
                     break;
                 case "ArrowRight":
                     this.right=false;
                     break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
                 case "ArrowDown":
                     this.reverse=false;
                     break;
             }
-            // console.table(this);
         }
-
     }
 }

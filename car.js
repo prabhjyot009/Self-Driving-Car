23ce7e7f3bf3ad9606979e1fb9e1e6de92ea5861
prabhjyot009 <prabh156dps@gmail.com>
2023-11-24 12:29:50 +0530

    Update car.js and main.js

--- car.js
+++ car.js
@@ -18,20 +18,25 @@ class Car {
         this.controls = new Controls(controlType);
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         if (!this.damaged) {
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
         if (this.sensor) {
-            this.sensor.update(roadBorders);
+            this.sensor.update(roadBorders, traffic);
         }
     }
 
-    #assessDamage(roadBorders) {
+    #assessDamage(roadBorders, traffic) {
         for (let i = 0; i < roadBorders.length; i++) {
             if (polysIntersect(this.polygon, roadBorders[i])) {
+                return true;
+            }
+        }
+        for (let i = 0; i < traffic.length; i++) {
+            if (polysIntersect(this.polygon, traffic[i].polygon)) {
                 return true;
             }
         }
@@ -99,12 +104,12 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx) {
+    draw(ctx,color) {
         if (this.damaged) {
-            ctx.fillStyle = "red";
+            ctx.fillStyle = "gray";
         }
         else {
-            ctx.fillStyle = "black";
+            ctx.fillStyle = color;
         }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
@@ -113,7 +118,6 @@ class Car {
         }
         ctx.fill();
         if (this.sensor) {
-
             this.sensor.draw(ctx);
         }
     }
